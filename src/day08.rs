c033dffbf575dfbba63ddb5238a27b46e9bddbfd
Max Larsson <maxslarsson@gmail.com>
2020-12-08 10:27:10 -0500

    Solved day 8! Also added a vscode config for debugging

---
+++ src/day08.rs
@@ -0,0 +1,134 @@
+use std::collections::HashMap;
+
+pub fn part1(input: String) {
+    let mut instructions: Vec<(&str, isize)> = Vec::new();
+
+    for instru in input.split("\n") {
+        let split: Vec<_> = instru.split(" ").collect();
+
+        assert_eq!(split.len(), 2);
+
+        instructions.push((split[0], split[1].parse().unwrap()));
+    }
+
+    let mut index: isize = 0;
+    let mut accumulator = 0;
+
+    let mut visited_indexes = Vec::new();
+
+    while !visited_indexes.contains(&index) {
+        visited_indexes.push(index);
+
+        let instruction = instructions[index as usize];
+
+        match instruction.0 {
+            "nop" => (),
+            "acc" => accumulator += instruction.1,
+            "jmp" => index += instruction.1,
+            _ => panic!("Unknown instruction!")
+        }
+
+        if instruction.0 != "jmp" {
+            index += 1;
+        }
+    }
+
+    println!("{}", accumulator);
+}
+
+pub fn part2(input: String) {
+    let mut instructions: Vec<(&str, isize)> = Vec::new();
+
+    for instru in input.split("\n") {
+        let split: Vec<_> = instru.split(" ").collect();
+
+        assert_eq!(split.len(), 2);
+        
+        instructions.push((split[0], split[1].parse().unwrap()));
+    }
+    
+    let index = 0;
+    let accumulator = 0;
+    
+    let mut visited_indexes = HashMap::new();
+
+    println!("{}", recursive(index, accumulator, visited_indexes, &instructions, false));
+
+    // while (index as usize) < instructions.len() {
+    //     let mut went_backwards = false; 
+    //     if visited_indexes.contains(&index) {
+    //         went_backwards = true;
+    //         loop {
+    //             index = visited_indexes.pop().unwrap();
+                
+    //             match instructions[index as usize].0 {
+    //                 "nop" | "jmp" => break,
+    //                 _ => ()
+    //             }
+    //         }
+    //     }
+
+    //     visited_indexes.push(index);
+        
+    //     let instruction = instructions[index as usize];
+        
+    //     if went_backwards {
+    //         match instruction.0 {
+    //             "nop" => index += instruction.1,
+    //             "jmp" => (),
+    //             "acc" => accumulator += instruction.1,
+    //             _ => panic!("Unknown instruction!")
+    //         }
+    //     } else {
+    //         match instruction.0 {
+    //             "nop" => (),
+    //             "jmp" => index += instruction.1,
+    //             "acc" => accumulator += instruction.1,
+    //             _ => panic!("Unknown instruction!")
+    //         }
+    //     }
+
+    //     if instruction.0 != "jmp" {
+    //         index += 1;
+    //     }
+    // }
+
+}
+
+fn recursive(index: isize, accumulator: isize, mut visited_indexes: HashMap<isize, usize>, instructions: &Vec<(&str, isize)>, changed: bool) -> isize {
+    if (index as usize) == instructions.len() {
+        return accumulator;
+    }
+
+    if let Some(count) = visited_indexes.get_mut(&index) {
+        if *count < 1 {
+            *count += 1;
+        } else {
+            return 0;
+        }
+    } else {
+        visited_indexes.insert(index, 1).unwrap_none();
+    }
+
+    let instruction = instructions[index as usize];
+
+    match instruction.0 {
+        "nop" => {
+            if changed {
+                return recursive(index + 1, accumulator, visited_indexes.clone(), instructions, changed) 
+            } else {
+                return recursive(index + 1, accumulator, visited_indexes.clone(), instructions, changed) + recursive(index + instruction.1, accumulator, visited_indexes.clone(), instructions, !changed)
+            }
+        },
+        "jmp" => {
+            if changed {
+                return recursive(index + instruction.1, accumulator, visited_indexes.clone(), instructions, changed)
+            } else {
+                return recursive(index + instruction.1, accumulator, visited_indexes.clone(), instructions, changed) + recursive(index + 1, accumulator, visited_indexes.clone(), instructions, !changed) 
+            }
+        },
+        "acc" => return recursive(index + 1, accumulator + instruction.1, visited_indexes.clone(), instructions, changed),
+        _ => panic!("Unknown instruction!")
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(option_unwrap_none)]
+
 // Days
 pub mod day01;
 pub mod day02;
@@ -6,6 +8,7 @@ pub mod day04;
 pub mod day05;
 pub mod day06;
 pub mod day07;
+pub mod day08;
 
 pub fn noop(_input: String) {}
 
@@ -20,6 +23,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         5 => (day05::part1, day05::part2),
         6 => (day06::part1, day06::part2),
         7 => (day07::part1, day07::part2),
+        8 => (day08::part1, day08::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
