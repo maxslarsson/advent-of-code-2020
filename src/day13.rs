2460caeb28b3fa8deabaaac936bca8988860ebb1
Max Larsson <maxslarsson@gmail.com>
2020-12-15 23:58:05 -0500

    Solved day 13 and day 14

--- src/day13.rs
+++ src/day13.rs
@@ -1,7 +1,3 @@
-use std::convert::TryInto;
-use num::Integer;
-use num::bigint::BigUint;
-
 pub fn part1(input: String) {
     let split: Vec<&str> = input.split("\n").collect();
     let earliest: usize = split[0].parse().unwrap();
@@ -30,21 +26,8 @@ pub fn part1(input: String) {
     }
 }
 
+// Extremely slow: Took 269m 26s 620ms 217µs
 pub fn part2(input: String) {
-    let split: Vec<&str> = input.split("\n").collect();
-
-    let iter: Vec<(usize, usize)> = split[1]
-        .split(",")
-        .enumerate()
-        .filter(|x| x.1 != "x")
-        .map(|x| (x.0, x.1.parse().unwrap()))
-        .collect();
-
-    let lcm = iter.iter().fold(1, |acc, x| acc.lcm(&x.1));
-    println!("{}", lcm, );
-}
-
-pub fn part2_slow(input: String) {
     let split: Vec<&str> = input.split("\n").collect();
     let earliest: usize = split[0].parse().unwrap();
 
@@ -54,7 +37,7 @@ pub fn part2_slow(input: String) {
         .filter(|x| x.1 != "x")
         .map(|x| (x.0, x.1.parse().unwrap()))
         .collect();
-    
+
     // Sort to get largest number first at index 0
     iter.sort_unstable_by_key(|x| x.1);
     iter.reverse();
@@ -69,68 +52,25 @@ pub fn part2_slow(input: String) {
 
     let len = iter.len();
 
-    // let pool = rayon::ThreadPoolBuilder::new().num_threads(num_cpus::get()).build().unwrap();
-    // pool.install(|| {
-        loop {
-            // Print timestamp every 50,000,000 numbers checked
-            if (timestamp + biggest.0) % (50_000_000 * biggest.1) == 0 {
-                println!("{}", timestamp);
-            }
-
-            // We know the first element is a multiple of the timestamp
-            // let found = iter[1..].par_iter().map(|(i, bus_id)| ((timestamp + i) % bus_id == 0).then_some(())).while_some().count();
-            // let found = iter[1..].iter().map(|(i, bus_id)| ((timestamp + i) % bus_id == 0).then_some(())).take_while(|x| x.is_some()).count();
-            let found = iter.iter().map(|(i, bus_id)| ((timestamp + i) % bus_id == 0).then_some(())).take_while(|x| x.is_some()).count();
-
-            // Minus 1 because iterator skips first element
-            if found == len - 1 {
-                break;
-            }
-
-            timestamp += biggest.1;
-        }
-    // });
-
-    println!("{}", timestamp);
-}
-
-pub fn part2_slowest(input: String) {
-    let split: Vec<&str> = input.split("\n").collect();
-    let earliest: usize = split[0].parse().unwrap();
-
-    let buses: Vec<&str> = split[1].split(",").collect();
-
-    // let mut timestamp = earliest;
-    let mut timestamp = 100_000_000_000_000;
-
-    let iter: Vec<(usize, usize)> = buses
-        .into_iter()
-        .enumerate()
-        .filter(|x| x.1 != "x")
-        .map(|x| (x.0, x.1.parse().unwrap()))
-        .collect();
-    let len = iter.len();
-    let mut found = 0;
-
-    while found == 0 {
-        if timestamp % 500_000_000 == 0 {
+    loop {
+        // Print timestamp every 50,000,000 numbers checked
+        if (timestamp + biggest.0) % (50_000_000 * biggest.1) == 0 {
             println!("{}", timestamp);
         }
 
-        for (i, bus_id) in &iter {
-            if (timestamp + i) % bus_id == 0 {
-                found += 1;
-            } else {
-                break;
-            }
-        }
+        // We know the first element is a multiple of the timestamp
+        let found = iter[1..]
+            .iter()
+            .map(|(i, bus_id)| ((timestamp + i) % bus_id == 0).then_some(()))
+            .take_while(|x| x.is_some())
+            .count();
 
-        if found == len {
+        // Minus 1 because iterator skips first element
+        if found == len - 1 {
             break;
         }
 
-        timestamp += 1;
-        found = 0;
+        timestamp += biggest.1;
     }
 
     println!("{}", timestamp);
---
+++ src/day14.rs
@@ -0,0 +1,94 @@
+use itertools::Itertools;
+use regex::Regex;
+use std::collections::HashMap;
+
+pub fn part1(input: String) {
+    // let mut memory = vec![0; 2usize.pow(36)];
+    // let mut memory = Vec::with_capacity(2usize.pow(36));
+    let mut memory = HashMap::new();
+    let re = Regex::new(r"mask = [X01]{36}(\nmem\[\d+\] = \d+)+").unwrap();
+
+    for mat in re.find_iter(&input) {
+        let lines: Vec<_> = mat.as_str().split("\n").collect();
+        let bitmask = &lines[0][7..];
+
+        for &mem in &lines[1..] {
+            let end = mem.find("=").unwrap();
+            let loc: usize = mem[4..end - 2].parse().unwrap();
+            let decimal: usize = mem[end + 2..].parse().unwrap();
+
+            let value = format!("{:036b}", decimal);
+            let result: String = value
+                .chars()
+                .zip(bitmask.chars())
+                .map(|c| if c.1 == 'X' { c.0 } else { c.1 })
+                .collect();
+
+            let decimal_result = usize::from_str_radix(&result, 2).unwrap();
+
+            memory.insert(loc, decimal_result);
+        }
+    }
+
+    let sum: usize = memory.values().sum();
+    println!("{}", sum);
+}
+
+pub fn part2(input: String) {
+    // let mut memory = vec![0; 2usize.pow(36)];
+    // let mut memory = Vec::with_capacity(2usize.pow(36));
+    let mut memory = HashMap::new();
+    let re = Regex::new(r"mask = [X01]{36}(\nmem\[\d+\] = \d+)+").unwrap();
+
+    for mat in re.find_iter(&input) {
+        let lines: Vec<_> = mat.as_str().split("\n").collect();
+        let bitmask = &lines[0][7..];
+
+        for &mem in &lines[1..] {
+            let end = mem.find("=").unwrap();
+            let loc: usize = mem[4..end - 2].parse().unwrap();
+            let decimal: usize = mem[end + 2..].parse().unwrap();
+
+            let address = format!("{:036b}", loc);
+            let result: String = address
+                .chars()
+                .zip(bitmask.chars())
+                .map(|c| if c.1 == '0' { c.0 } else { c.1 })
+                .collect();
+
+            let indexes_of_x: Vec<usize> = result
+                .chars()
+                .enumerate()
+                .filter(|c| c.1 == 'X')
+                .map(|c| c.0)
+                .collect();
+
+            let addresses: Vec<String> = vec!["01".chars(); indexes_of_x.len()]
+                .into_iter()
+                .multi_cartesian_product()
+                .map(|x| {
+                    result
+                        .chars()
+                        .enumerate()
+                        .map(|c| {
+                            if let Some(i) = indexes_of_x.iter().position(|&cc| cc == c.0) {
+                                x[i]
+                            } else {
+                                c.1
+                            }
+                        })
+                        .collect()
+                })
+                .collect();
+
+            for address in addresses.iter() {
+                let address = usize::from_str_radix(address, 2).unwrap();
+
+                memory.insert(address, decimal);
+            }
+        }
+    }
+
+    let sum: usize = memory.values().sum();
+    println!("{}", sum);
+}
