4872f068777bb605aaf8f00110cf2c8248fab2f6
Max Larsson <maxslarsson@gmail.com>
2020-12-19 04:10:17 -0500

    Buggy code but working on day 16

--- src/day16.rs
+++ src/day16.rs
@@ -23,5 +23,27 @@ pub fn part1(input: String) {
     assert_eq!(your_tickets.len(), 1);
     let your_ticket = your_tickets[0];
 
-    let neaby_tickets: Vec<&str> = parts[1].split('\n').skip(1).collect();
+    let nearby_tickets: Vec<Vec<usize>> = parts[2].split('\n').skip(1).map(|ticket| ticket.split(',').map(|num| num.parse().unwrap()).collect()).collect();
+
+    let mut invalid_nums = Vec::new();
+
+    for ticket in nearby_tickets {
+        for num in ticket {
+            let mut count = 0;
+           for field in fields.values() {
+               if (num < field.0.0 || num > field.0.1) && (num < field.1.0 || num > field.1.1) {
+                   count += 1;
+               }
+           }
+
+            let max = fields.values().count();
+
+            if count == max - 1 {
+                invalid_nums.push(num);
+            }
+        }
+    }
+
+    let sum: usize = invalid_nums.iter().sum();
+    println!("{}", sum)
 }
